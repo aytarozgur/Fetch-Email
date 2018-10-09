056061340f06446ce6765a63321ec53ad6a1c9d6
Özgür Hasan Aytar <zrhaytar@gmail.com>
2018-10-09 15:51:14 +0200

    Uploaded

---
+++ src/main/java/edu/bim313/App.java
@@ -0,0 +1,32 @@
+package edu.bim313;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class App {
+
+    public static void main(String[] args) throws IOException {
+
+        String url = args[0];
+//        Scanner input = new Scanner(System.in);
+//        String url = input.nextLine();
+        //"http://www.kimya.anadolu.edu.tr/OgretimElemanlari.aspx";
+        System.out.println("Fetching: " + url);
+
+        Document doc = Jsoup.connect(url).get();
+        Elements links = doc.select("a[href]");
+
+
+        for (Element link : links) {
+            String s = link.attr("abs:href").trim();
+
+            if (s.startsWith("mailto:"))
+                System.out.println(s.substring(7));
+        }
+    }
+}
